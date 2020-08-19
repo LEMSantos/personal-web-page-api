fd7cf8338620fc61923ddf84726f64405632dac4
LEMSantos <lucas_m-santos@hotmail.com>
2020-08-19 20:47:25 -0300

    Add - finaliza os testes do cli

--- tests/cli/test_cli_commands.py
+++ tests/cli/test_cli_commands.py
@@ -1,8 +1,11 @@
 import mock
 import pytest
 import env
+import click
 from click.testing import CliRunner
+from tests.factories import factory
 from personalwebpageapi.cli import cli
+from personalwebpageapi.models.auth import Auth
 
 
 @pytest.fixture
@@ -30,3 +33,29 @@ def test_cli_prod_command_without_command(mocker, runner):
 
     assert result.exit_code == 0
     mocker.assert_called_once()
+
+
+def test_cli_token_command_without_params(runner):
+    result = runner.invoke(cli, ['token'])
+    assert 'Error: no option provided' in result.output
+
+
+def test_cli_token_list(runner):
+    factory(Auth).create(token='token_1')
+    factory(Auth).create(token='token_2')
+
+    result = runner.invoke(cli, ['token', '-l'])
+
+    assert result.exit_code == 0
+    assert result.output == 'token_1\ntoken_2\n'
+
+
+@mock.patch('personalwebpageapi.cli.token_generator')
+def test_cli_token_create(mocker, runner):
+    mocker.return_value = 'test_token'
+
+    result = runner.invoke(cli, ['token', '-c'])
+    auth_token = Auth.where('token', 'test_token').get()
+
+    assert auth_token
+    mocker.assert_called_with(100)
