deb6254be55848b433a7cc4b2a121c58d5544d4c
LEMSantos <lucas_m-santos@hotmail.com>
2020-09-07 10:15:31 -0300

    Add - factory para criação de rascunhos

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -4,14 +4,21 @@ from personalwebpageapi.models.post import Post
 from personalwebpageapi.models.post_content import PostContent
 from personalwebpageapi.models.auth import Auth
 
-from tests.factories.post import post_factory
+from tests.factories.post import post_factory, post_is_draft_factory
 from tests.factories.post_content import post_content_factory
 from tests.factories.auth import auth_factory
+
+from functools import partial
 
 factory = Factory()
 
 # Post
 factory.register(Post, post_factory)
+factory.register_as(
+    Post,
+    'is_draft',
+    partial(post_is_draft_factory, factory),
+)
 
 # PostContent
 factory.register(PostContent, post_content_factory)
